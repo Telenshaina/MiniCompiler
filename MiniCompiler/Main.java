775f285c79f85a78d304485083ffd4065c80a50d
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2024-11-20 02:08:05 +0800

    Update Main.java
    - Added UI colors, including assigned colors for enabled and disabled buttons
    - Buttons are rounded through the RoundedButton class
    - File acceptable is only *.txt format, will have a pop-up error otherwise
    - Extracted the following:
    1. UIHelper class: JLabel and JButton creation
    2. ButtonActionHandler class: Action Listeners of the buttons

--- MiniCompiler/Main.java
+++ MiniCompiler/Main.java
@@ -1,13 +1,10 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
+import javax.swing.*;
 
 public class Main extends JFrame {
     private JLabel phases, result;
     private JTextArea resultArea, fileArea;
     private JButton lexicalButton, syntaxButton, semanticButton, openButton, clearButton;
-    private JPanel mainPanel;
 
     public Main() {
         initComponents();
@@ -15,22 +12,22 @@ public class Main extends JFrame {
 
     // Initializing UI Components
     private void initComponents() {
-        setTitle("File Reader");
+        setTitle("Java Mini Compiler");
         setSize(new Dimension(1280, 720));
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        mainPanel = mainPanel();
-        add(mainPanel);
+        add(mainPanel());
     }
 
     // mainPanel serves as a container
     public JPanel mainPanel() {
         JPanel panel = new JPanel(null);
         panel.setBounds(0, 0, 1280, 720);
-        panel.setBackground(new Color(0XF6F6F6));
+        panel.setBackground(new Color(0xF6D4EB));
         panel.add(contentPanel());
+
         return panel;
     }
 
@@ -38,113 +35,71 @@ public class Main extends JFrame {
     public JPanel contentPanel() {
         JPanel panel = new JPanel(null);
         panel.setBounds(30, 30, 1200, 620);
-        // panel.setBackground(Color.PINK);
-        panel.setBackground(new Color(0xF6F6F6));
+        panel.setBackground(new Color(0xF6D4EB));
+
+        
 
         // Compiler Phases Buttons
-        phases = new JLabel("Phases:");
-        phases.setBounds(20, 5, 300, 60);
-        phases.setForeground(Color.BLACK);
-        phases.setFont(new Font("Arial", Font.BOLD, 15));
+        phases = UIHelper.createLabel("Phases:", 20, 45, 75, 30);
         panel.add(phases);
 
-        lexicalButton = new JButton("Lexical Analysis");
-        lexicalButton.setBounds(20, 45, 300, 60);
-        lexicalButton.setBackground(Color.BLACK);
-        lexicalButton.setForeground(Color.WHITE);
-        lexicalButton.setFont(new Font("Arial", Font.BOLD, 12));
-        lexicalButton.setFocusable(false);
+        lexicalButton = UIHelper.createButton("Lexical Analysis", 20, 75, 300, 60);
+        lexicalButton.setEnabled(false);
         panel.add(lexicalButton);
 
-        syntaxButton = new JButton("Syntax Analysis");
-        syntaxButton.setBounds(450, 45, 300, 60);
-        syntaxButton.setBackground(Color.BLACK);
-        syntaxButton.setForeground(Color.WHITE);
-        syntaxButton.setFont(new Font("Arial", Font.BOLD, 12));
-        syntaxButton.setFocusable(false);
+        syntaxButton = UIHelper.createButton("Syntax Analysis", 450, 75, 300, 60);
+        syntaxButton.setEnabled(false);
         panel.add(syntaxButton);
 
-        semanticButton = new JButton("Semantic Analysis");
-        semanticButton.setBounds(880, 45, 300, 60);
-        semanticButton.setBackground(Color.BLACK);
-        semanticButton.setForeground(Color.WHITE);
-        semanticButton.setFont(new Font("Arial", Font.BOLD, 12));
-        semanticButton.setFocusable(false);
+        semanticButton = UIHelper.createButton("Semantic Analysis", 880, 75, 300, 60);
+        semanticButton.setEnabled(false);
         panel.add(semanticButton);
 
         // Result text area
-        result = new JLabel("Result:");
-        result.setBounds(20, 120, 300, 60);
-        result.setForeground(Color.BLACK);
-        result.setFont(new Font("Arial", Font.BOLD, 15));
+        result = UIHelper.createLabel("Result:", 20, 160, 75, 30);
 
         resultArea = new JTextArea();
-        resultArea.setBounds(20, 160, 300, 180);
+        resultArea.setBounds(20, 190, 300, 180);
         resultArea.setEditable(false);
-        resultArea.setFont(new Font("Arial", Font.PLAIN, 14));
+        resultArea.setFont(new Font("Verdana", Font.BOLD, 16));
+        resultArea.setBorder(BorderFactory.createLineBorder(new Color(0xD83E76), 1)); // Add border
         JScrollPane scrollPane1 = new JScrollPane(resultArea);
-        scrollPane1.setBounds(20, 160, 300, 180);
+        scrollPane1.setBounds(20, 190, 300, 180);
 
         panel.add(result);
         panel.add(scrollPane1);
 
         // Open File Button
-        openButton = new JButton("Open File");
-        openButton.setBounds(20, 420, 300, 60);
-        openButton.setBackground(new Color(0xD83E76)); // Initial color for enabled state
-        openButton.setForeground(Color.WHITE);
-        openButton.setFont(new Font("Arial", Font.BOLD, 12));
-        openButton.setFocusable(false);
+        openButton = UIHelper.createButton("Open File", 20, 450, 300, 60);
         panel.add(openButton);
 
         // Clear Button
-        clearButton = new JButton("Clear");
-        clearButton.setBounds(20, 500, 300, 60);
-        clearButton.setBackground(new Color(0xD83E76));
-        clearButton.setForeground(Color.WHITE);
-        clearButton.setFont(new Font("Arial", Font.BOLD, 12));
-        clearButton.setFocusable(false);
+        clearButton = UIHelper.createButton("Clear", 20, 530, 300, 60);
         clearButton.setEnabled(false); // Disabled by default, even with the default instructions is displayed
         panel.add(clearButton);
 
         // Open file area
         fileArea = new JTextArea();
-        fileArea.setBounds(380, 160, 800, 400);
+        fileArea.setBounds(380, 190, 800, 400);
         fileArea.setEditable(false);
-        fileArea.setFont(new Font("Courier New", Font.PLAIN, 14));
+        fileArea.setFont(new Font("Courier New", Font.PLAIN, 16));
+        fileArea.setBorder(BorderFactory.createLineBorder(new Color(0xD83E76), 1)); // Add border
 
         // Default instructions
         fileArea.setText(DefaultInstructions.getInstructions());
         
         JScrollPane scrollPane2 = new JScrollPane(fileArea);
-        scrollPane2.setBounds(380, 160, 800, 400);
+        scrollPane2.setBounds(380, 190, 800, 400);
         panel.add(scrollPane2);
 
-        openButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                int result = fileChooser.showOpenDialog(Main.this);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File file = fileChooser.getSelectedFile();
-                    FileReaderUtility fileReaderUtility = new FileReaderUtility(fileArea); // Calls readFile in the FileReaderUtility class
-                    fileReaderUtility.readFile(file);
-                    openButton.setEnabled(false); // Disable the button after opening a file
-                    clearButton.setEnabled(true); // Enable clearButton after file is opened
-                    
-                }
-            }
-        });
+        ButtonActionHandler actionHandler = new ButtonActionHandler(this, fileArea, resultArea, openButton, clearButton, 
+                                                                    lexicalButton, syntaxButton, semanticButton);
+        openButton.addActionListener(actionHandler);
+        clearButton.addActionListener(actionHandler);
+        lexicalButton.addActionListener(actionHandler);
+        syntaxButton.addActionListener(actionHandler);
+        semanticButton.addActionListener(actionHandler);
 
-        clearButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                fileArea.setText(""); // Clears the text in fileArea
-                openButton.setEnabled(true); // Re-enables the openButton to choose a new file
-                openButton.setBackground(new Color(0xD83E76)); // Restore enabled color 
-                clearButton.setEnabled(false); // Disable clearButton when no file
-            }
-        });
         return panel;
     }
 
